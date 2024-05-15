460cfd1352170db92e272b3a306ec3cc4cf66700
Piyush <piyushdravyakar48@gmail.com>
2024-05-15 19:33:04 +0530

    same

--- TestCases/test_myInfo_page.py
+++ TestCases/test_myInfo_page.py
@@ -25,7 +25,7 @@ class Test_MyInfo:
         # assert self.driver == "OrangeHRM", " fail test"
         if self.driver.title == "OrangeHRM":
             self.logs.info("*********your test is pass**********")
-            print("from main to piyush")
+            print("from main to piyush2")
         else:
             self.logs.error("***********Your test is fail***************")
             self.driver.save_screenshot(r"C:\Users\ASUS\PycharmProjects\rahulshettycodes\Screenshots\info.png")
