a62d551ad2621bbb33076e933af2400df2e3986b
Piyush <piyushdravyakar48@gmail.com>
2024-05-15 13:54:58 +0530

    readproperties

---
+++ Utilities/readProperties.py
@@ -0,0 +1,22 @@
+import configparser
+
+config = configparser.RawConfigParser()
+
+config.read(r"C:\Users\ASUS\PycharmProjects\rahulshettycodes\Configuration\config.ini")
+
+class ReadProperties:
+
+    @staticmethod
+    def userfname():
+        name = config.get("common info","name")
+        return name
+
+    @staticmethod
+    def usermname():
+        mname = config.get("common info", "mname")
+        return mname
+
+    @staticmethod
+    def userlnamee():
+        lname = config.get("common info", "lname")
+        return lname
