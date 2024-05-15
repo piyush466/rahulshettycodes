598ed0f31a633ecafe8f7ab45a922cd1afdbf36d
Piyush <piyushdravyakar48@gmail.com>
2024-05-15 16:47:55 +0530

    logfile changes

--- Utilities/logging.py
+++ Utilities/logging.py
@@ -1,17 +1,31 @@
 import logging
+#
+# class logGen:
+#
+#     @staticmethod
+#     def logger():
+#
+#         logger = logging.getLogger("piyush")
+#         logger.setLevel(logging.DEBUG)
+#         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+#         filehandler = logging.FileHandler(r"C:\Users\ASUS\PycharmProjects\rahulshettycodes\Logs\Generate.log")
+#         filehandler.setLevel(logging.INFO)
+#         filehandler.setFormatter(formatter)
+#         logger.addHandler(filehandler)
+#
+#         return logger
 
+import logging
 class logGen:
-
     @staticmethod
     def logger():
-
         logger = logging.getLogger("piyush")
-        logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        filehandler = logging.FileHandler(r"C:\Users\ASUS\PycharmProjects\rahulshettycodes\Logs\Generatelog.log")
-        filehandler.setLevel(logging.INFO)
-        filehandler.setFormatter(formatter)
-        logger.addHandler(filehandler)
-
+        if not logger.handlers:  # Check if a FileHandler has already been added
+            logger.setLevel(logging.DEBUG)
+            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+            filehandler = logging.FileHandler(r"C:\Users\ASUS\PycharmProjects\rahulshettycodes\Logs\Generate.log")
+            filehandler.setLevel(logging.INFO)
+            filehandler.setFormatter(formatter)
+            logger.addHandler(filehandler)
         return logger
 
